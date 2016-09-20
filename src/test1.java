bb629cb4ba9f54b4c023f981b04bb98aae534d01
Johans <duanqingbb@163.com>
2016-09-20 08:59:18 +0800

    Initial the project

---
+++ src/test1.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+
+public class test1 {
+	
+	public static void main(String[] args) {
+		JFrame JF = new JFrame();
+		//设置标题
+		JF.setTitle("登录窗口");
+		JF.setBounds(200, 200, 300, 300);
+		//设置当前窗口的可见性
+		
+	}
+
+}
